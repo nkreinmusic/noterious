913b67f07fe2e1dac6518f5ac04c4c467332cb8c
Nick Krein <nkreinmusic@gmail.com>
2016-04-26 15:33:24 -0700

    fix firebase endpoint

--- src/app/noterious.js
+++ src/app/noterious.js
@@ -6,7 +6,7 @@ angular.module('noterious', [
   'firebase',
   'noterious.common'
 ])
-  .constant('ENDPOINT_URI', 'https://noterious.firebaseio.com/')
+  .constant('ENDPOINT_URI', 'https://frontendmasters-noterious.firebaseio.com/')
   .config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/boards');
 
